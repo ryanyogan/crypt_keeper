9076b3199adf15457645e0ac5a99ffcd6afe5f3f
ryan <ryanyogan@gmail.com>
2021-11-09 16:02:12 -0600

    save state in the uri

--- lib/crypt_keeper_web/live/crypto_dashboard_live.ex
+++ lib/crypt_keeper_web/live/crypto_dashboard_live.ex
@@ -2,6 +2,7 @@ defmodule CryptKeeperWeb.CryptoDashboardLive do
   @moduledoc false
   use CryptKeeperWeb, :live_view
   alias CryptKeeper.Product
+  alias CryptKeeperWeb.Router.Helpers, as: Routes
   import CryptKeeperWeb.ProductHelpers
   require Logger
 
@@ -16,28 +17,44 @@ defmodule CryptKeeperWeb.CryptoDashboardLive do
     {:ok, socket}
   end
 
+  @impl true
+  def handle_params(%{"products" => product_ids}, _uri, socket) do
+    new_products = Enum.map(product_ids, &product_from_string/1)
+    diff = List.myers_difference(socket.assigns.products, new_products)
+
+    products_to_remove =
+      diff
+      |> Keyword.get_values(:del)
+      |> List.flatten()
+
+    products_to_insert =
+      diff
+      |> Keyword.get_values(:ins)
+      |> List.flatten()
+
+    socket =
+      Enum.reduce(products_to_remove, socket, fn product, socket ->
+        remove_product(socket, product)
+      end)
+
+    socket =
+      Enum.reduce(products_to_insert, socket, fn product, socket ->
+        add_product(socket, product)
+      end)
+
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_params(_params, _uri, socket) do
+    {:noreply, socket}
+  end
+
   defp get_timezone_from_connection(socket) do
     case get_connect_params(socket) do
       %{"timezone" => tz} when not is_nil(tz) -> tz
       _ -> "UTC"
     end
-  end
-
-  @impl true
-  def handle_params(%{"product_id" => product_id}, _uri, socket) do
-    product = product_from_string(product_id)
-
-    socket =
-      socket
-      |> assign(:selected_product, product)
-      |> maybe_update_title_with_trade(CryptKeeper.get_last_trade(product))
-
-    {:noreply, socket}
-  end
-
-  def handle_params(params, _uri, socket) do
-    Logger.debug("catch-all handle_params: #{inspect(params)}")
-    {:noreply, socket}
   end
 
   @impl true
@@ -48,17 +65,19 @@ defmodule CryptKeeperWeb.CryptoDashboardLive do
       trade: trade
     )
 
-    socket =
-      socket
-      |> maybe_update_title_with_trade(trade)
-
     {:noreply, socket}
   end
 
   @impl true
   def handle_event("add-product", %{"product_id" => product_id}, socket) do
-    product = product_from_string(product_id)
-    socket = maybe_add_product(socket, product)
+    product_ids =
+      socket.assigns.products
+      |> Enum.map(&to_string/1)
+      |> Kernel.++([product_id])
+      |> Enum.uniq()
+
+    socket = push_patch(socket, to: Routes.live_path(socket, __MODULE__, products: product_ids))
+
     {:noreply, socket}
   end
 
@@ -67,29 +86,16 @@ defmodule CryptKeeperWeb.CryptoDashboardLive do
   end
 
   def handle_event("remove-product", %{"product-id" => product_id}, socket) do
-    product = product_from_string(product_id)
-    socket = update(socket, :products, &List.delete(&1, product))
-    CryptKeeper.unsubscribe_to_trades(product)
+    product_ids =
+      socket.assigns.products
+      |> Enum.map(&to_string/1)
+      |> Kernel.--([product_id])
+      |> Enum.uniq()
+
+    socket = push_patch(socket, to: Routes.live_path(socket, __MODULE__, products: product_ids))
+
     {:noreply, socket}
   end
-
-  defp maybe_add_product(socket, product) do
-    if product in socket.assigns.products do
-      socket
-    else
-      socket
-      |> add_product(product)
-    end
-  end
-
-  defp maybe_update_title_with_trade(
-         %{assigns: %{selected_product: product}} = socket,
-         %{product: product} = trade
-       ) do
-    assign(socket, :page_title, "#{trade.price} - #{product.currency_pair}")
-  end
-
-  defp maybe_update_title_with_trade(socket, _trade), do: socket
 
   defp product_from_string(product_id) do
     [exchange_name, currency_pair] = String.split(product_id, ":")
@@ -100,6 +106,13 @@ defmodule CryptKeeperWeb.CryptoDashboardLive do
     CryptKeeper.subscribe_to_trades(product)
 
     socket
-    |> update(:products, fn products -> [product | products] end)
+    |> update(:products, &(&1 ++ [product]))
+  end
+
+  defp remove_product(socket, product) do
+    CryptKeeper.unsubscribe_to_trades(product)
+
+    socket
+    |> update(:products, &(&1 -- [product]))
   end
 end
